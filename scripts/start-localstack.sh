aa27e0e4eacb5aa0dd4c0e10a22b09feb76259c2
Emanuelle Schiavon <emanuelleschiavon@gmail.com>
2024-08-21 09:58:24 -0300

    Add sqs to get message

--- scripts/start-localstack.sh
+++ scripts/start-localstack.sh
@@ -4,4 +4,7 @@ set -euo pipefail
 
 echo "Creating topics"
 awslocal --endpoint-url=http://localhost:4566 sns create-topic --name=transaction-done --region=us-east-1
+awslocal --endpoint-url=http://localhost:4566 sqs create-queue --queue-name=queue-test --region=us-east-1
+awslocal sns subscribe --topic-arn arn:aws:sns:us-east-1:000000000000:transaction-done \
+ --protocol sqs --notification-endpoint arn:aws:sqs:us-east-1:000000000000:queue-test
 echo "Created topics"
