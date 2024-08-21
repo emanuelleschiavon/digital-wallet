69b01a176f60d10fe642f36379ed27b0964e7005
Emanuelle Schiavon <emanuelleschiavon@gmail.com>
2024-08-21 12:45:12 -0300

    Use containers dependencies

--- src/main/kotlin/com/picpay/wallet/infra/TransactionPublisher.kt
+++ src/main/kotlin/com/picpay/wallet/infra/TransactionPublisher.kt
@@ -2,6 +2,7 @@ package com.picpay.wallet.infra
 
 import com.amazonaws.services.sns.AmazonSNS
 import com.picpay.wallet.domain.Transaction
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
@@ -11,8 +12,12 @@ class TransactionPublisher(
 ) {
     @Value("\${cloud.aws.topic.transaction-done}")
     lateinit var transactionDoneTopic: String
+
+    private val logger = LoggerFactory.getLogger(TransactionPublisher::class.java)
+
     fun publish(transaction: Transaction): Transaction {
         snsClient.publish(transactionDoneTopic, transaction.toString())
+        logger.info("Sent message: $transaction")
         return transaction
     }
 }
